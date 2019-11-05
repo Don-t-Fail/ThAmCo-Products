7531980c61d243db1dc1c5894059c8e967e0b76b
Stedoss <29103029+Stedoss@users.noreply.github.com>
2019-11-05 14:07:52 +0000

    Add three basic classes for products.

---
+++ ThAmCo.Products/Data/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Products.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Brand Brand { get; set; }
+        public Category Category { get; set; }
+        public bool Active { get; set; }
+    }
+}
