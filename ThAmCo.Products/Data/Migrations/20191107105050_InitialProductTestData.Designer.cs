3f93121671f967920c8d8063ed6c45a90863bd54
Steven <29103029+Stedoss@users.noreply.github.com>
2019-11-07 10:54:12 +0000

    Create migration based on new test data.

--- ThAmCo.Products/Data/Migrations/20191107105050_InitialProductTestData.Designer.cs
+++ ThAmCo.Products/Data/Migrations/20191107105050_InitialProductTestData.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ThAmCo.Products.Data;
 
 namespace ThAmCo.Products.Data.Migrations
 {
     [DbContext(typeof(ProductsDbContext))]
-    partial class ProductsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191107105050_InitialProductTestData")]
+    partial class InitialProductTestData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
