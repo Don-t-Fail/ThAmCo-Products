1dc9056fee7110a6e122ef7921afcb7c3b5b276c
Stedoss <29103029+Stedoss@users.noreply.github.com>
2019-12-05 17:01:26 +0000

    Create test project with template controller tests.

---
+++ ThAmCo.Products.Tests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThAmCo.Products.Tests.Controllers
+{
+    [TestClass]
+    class ProductsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
