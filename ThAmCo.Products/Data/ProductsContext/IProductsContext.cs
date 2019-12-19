f21629ac4dbe43fda25e9235ffd5a60459e0f2ab
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-12-19 02:44:57 +0000

    Add Contexts for Context Dependency Injection.

---
+++ ThAmCo.Products/Data/ProductsContext/IProductsContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Products.Data.ProductsContext
+{
+    public interface IProductsContext
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetProductAsync(int id);
+        void AddProductAsync(Product product);
+        void SoftDeleteProductAsync(Product product = null, int? id = null);
+        void SaveAndUpdateContext();
+    }
+}
---
+++ ThAmCo.Products/Data/ProductsContext/MockProductsContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ThAmCo.Products.Data.ProductsContext
+{
+    public class MockProductsContext : IProductsContext
+    {
+        private List<Product> _products;
+
+        public MockProductsContext(List<Product> context)
+        {
+            _products = context;
+        }
+        
+        public Task<IEnumerable<Product>> GetAll()
+        {
+            return Task.FromResult(_products.AsEnumerable());
+        }
+
+        public Task<Product> GetProductAsync(int id)
+        {
+            return Task.FromResult(_products.FirstOrDefault(p => p.Id == id));
+        }
+
+        public void AddProductAsync(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public async void SoftDeleteProductAsync(Product product = null, int? id = null)
+        {
+            var chosenId = 0;
+            if (product == null && id != null)
+                chosenId = id ?? 0;
+            if (product != null && id == null)
+                chosenId = product.Id;
+            var productFromList = _products.FirstOrDefault(p => p.Id == chosenId);
+            if (productFromList != null)
+            {
+                _products.Remove(product);
+                _products.Add(product);
+            }
+        }
+
+        public async void SaveAndUpdateContext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
