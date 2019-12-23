cd270a15ed205add12ff41e281d47b46f6dab875
Steven <29103029+Stedoss@users.noreply.github.com>
2019-12-23 01:44:36 +0000

    Add Database Migration for new Brand and Category changes.

--- ThAmCo.Products/Data/Migrations/20191223014356_NewBrandAndCategoryObjects.Designer.cs
+++ ThAmCo.Products/Data/Migrations/20191223014356_NewBrandAndCategoryObjects.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ThAmCo.Products.Data;
 
 namespace ThAmCo.Products.Data.Migrations
 {
     [DbContext(typeof(ProductsDbContext))]
-    partial class ProductsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191223014356_NewBrandAndCategoryObjects")]
+    partial class NewBrandAndCategoryObjects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
