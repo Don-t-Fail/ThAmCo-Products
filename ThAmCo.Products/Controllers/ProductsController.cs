6e6646fd36fbf3296608b8c5507f4bc5f3a6f942
Stedos <29103029+Stedoss@users.noreply.github.com>
2020-01-07 12:57:48 +0000

    Remove functions from controller.

--- ThAmCo.Products/Controllers/ProductsController.cs
+++ ThAmCo.Products/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ThAmCo.Products.Data;
@@ -27,6 +28,7 @@ namespace ThAmCo.Products.Controllers
         }
 
         // GET: Products
+        //Auth here?
         public async Task<IActionResult> Index(double? PriceLow, double? PriceHigh, string Name, string Description, int BrandId = 0, int CategoryId = 0)
         {
             var products = await _context.GetAllActive();
@@ -109,71 +111,6 @@ namespace ThAmCo.Products.Controllers
             return View(new DetailsWithReviewsModelcs { Product = product, Reviews = objectResult });
         }
 
-        // GET: Products/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Products/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,Active")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.AddProductAsync(product);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);
-        }
-
-        // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-                return NotFound();
-
-            var product = await _context.GetProductAsync(id ?? 0);
-
-            if (product == null)
-                return NotFound();
-
-            return View(product);
-        }
-
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Active")] Product product)
-        {
-            throw new NotImplementedException();
-            /*if (id != product.Id)
-                return NotFound();
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.Id))
-                        return NotFound();
-                    else
-                        throw;
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);*/
-        }
-
         // GET: Products/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -189,6 +126,7 @@ namespace ThAmCo.Products.Controllers
         }
 
         // POST: Products/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
